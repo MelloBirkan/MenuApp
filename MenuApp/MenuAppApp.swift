6239f0c58e51e8221763801e77bbeafea91c0a3d
Mello Birkan <mgbirkan@icloud.com>
2024-03-18 15:50:15 -0300

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Marcello Gonzatto Birkan on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
